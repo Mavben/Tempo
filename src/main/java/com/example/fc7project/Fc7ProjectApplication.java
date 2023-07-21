aa2ab18c4dada3002d994197c63b2f313b5dcc49
mav <evrok907@gmail.com>
2023-07-22 08:14:47 +0900

    first commit

---
+++ src/main/java/com/example/fc7project/Fc7ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.fc7project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fc7ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fc7ProjectApplication.class, args);
+    }
+
+}
